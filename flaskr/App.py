18aaff0a614705bdc8a4be9dcc36df7c6badd261
Aprendiz <Aprendiz@BOGDFPCSRFOD113.FormacionSena.RED>
2023-08-25 11:43:15 -0500

    APIS Parte II

--- flaskr/App.py
+++ flaskr/App.py
@@ -1,6 +1,7 @@
 from flaskr import create_app
 from .Modelos import db, Cancion, Album, Usuario, Medio
-from .Modelos import AlbumSchema
+from flask_restful import Api
+from .Vistas import VistaCanciones, VistaCancion
 
 app = create_app('default')
 app_context = app.app_context()
@@ -9,9 +10,13 @@ app_context.push()
 db.init_app(app)
 db.create_all()
 
-with app.app_context():
-    Album_Schema = AlbumSchema()
-    A = Album(titulo='test', anio='1999', descripcion='#', medio=Medio.cd)
-    db.session.add(A)
-    db.session.commit()
-    print([Album_Schema.dumps(album) for album in Album.query.all()])
+api = Api(app)
+api.add_resource(VistaCanciones, '/canciones')
+api.add_resource(VistaCancion, '/canciones/<int:id_cancion>')
+
+#with app.app_context():
+    #Album_Schema = AlbumSchema()
+    #A = Album(titulo='test', anio='1999', descripcion='#', medio=Medio.cd)
+    #db.session.add(A)
+    #db.session.commit()
+    #print([Album_Schema.dumps(album) for album in Album.query.all()])
